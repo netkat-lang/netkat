1325e7f916bfc1d622882a249529b9c617067bab
cakoch10 <cakoch10@gmail.com>
2019-07-23 17:40:21 -0400

    added specs

--- src/kat/idd_compiler.mli
+++ src/kat/idd_compiler.mli
@@ -3,8 +3,16 @@ open Idds
 type b = Bdd.t
 type k = Idd.t
 
+(** [compile_bexp mgr interp_fields bexp] is the BDD representing [bexp] where 
+    [interp_fields] maps test names to their respective DD variable indices.
+    This mapping is required to be injective (or else the result is undefined).
+*)
 val compile_bexp : Bdd.manager -> interp_fields:(string -> int) 
   -> Packet_ast.bexp -> b
 
+(** [compile_exp mgr interp_fields exp] is the IDD representing [exp] where 
+    [interp_fields] maps test names to their respective DD variable indices.
+    This mapping is required to be injective (or else the result is undefined).
+*)
 val compile_exp : Idd.manager -> interp_fields:(string -> int) 
   -> Packet_ast.exp -> k
