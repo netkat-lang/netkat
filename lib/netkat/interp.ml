76ae5938ed7d6504623ad600e4c83c6a2731c558
Jedidiah McClurg <jrmcclurg@gmail.com>
2026-07-19 11:59:03 -0600

    keep track of witness trace for successful inequality check

--- lib/netkat/interp.ml
+++ lib/netkat/interp.ml
@@ -1,6 +1,6 @@
 open Nkcmd
 
-type result = Success | Fail of Trace.t option
+type result = Success of Trace.t option | Fail of Trace.t option
 
 let merge = (fun k v1 v2 -> Some(Value.S.union v1 v2))
 
@@ -18,13 +18,14 @@ let map_add m f v =
     Some(Field.M.add f (Value.S.add v st) m)
 
 let result_list_to_json rl = 
+  let str_trace t =
+    (Option.fold ~none:"" ~some:(fun t ->
+      ", \"trace\":["^(fst (List.fold_left (fun (acc,flag) pk -> (acc^(if flag then "" else ", ")^(Pk.to_json pk), false)) ("",true) t))^"]") t
+    ) in
   let item r = match r with
-  | Success -> "{\"result\":\"SUCCESS\"}"
+  | Success(t) -> Printf.sprintf "{\"result\":\"SUCCESS\"%s}" (str_trace t)
   | Fail(t) ->
-    Printf.sprintf "{\"result\":\"FAIL\"%s}"
-      (Option.fold ~none:"" ~some:(fun t ->
-        ", \"trace\":["^(fst (List.fold_left (fun (acc,flag) pk -> (acc^(if flag then "" else ", ")^(Pk.to_json pk), false)) ("",true) t))^"]") t
-      )
+    Printf.sprintf "{\"result\":\"FAIL\"%s}" (str_trace t)
   in
   "[\n"^(fst (List.fold_left (fun (acc,flag) r -> (acc^(if flag then "" else ",\n")^(item r), false)) ("",true) rl))^"\n]"
 
@@ -231,11 +232,15 @@ and interp out (bn: string) ((env: Env.t), (m: Value.S.t Field.M.t option)) (c: 
                          *)
                          ((env,snd l2), 
                          match b, res with
-                         | true, None
-                         | false, Some _ ->
+                         | true, None -> 
                            printf out "## *** Check \u{001b}[32mSUCCESS!\u{001b}[0m *** (%s %s %s) time: %fs\n%!"
                             (Nkexp.to_string e1) sgn (Nkexp.to_string e2) (stop -. start);
-                           [Success]
+                           [Success(None)]
+                         | false, Some cex ->
+                           printf out "## *** Check \u{001b}[32mSUCCESS!\u{001b}[0m *** (%s %s %s) time: %fs\n%!"
+                             (Nkexp.to_string e1) sgn (Nkexp.to_string e2) (stop -. start);
+                           printf out "Witness trace:\n%s\n%!" (Trace.to_string cex);
+                           [Success(Some(cex))]
                          | true, Some cex ->
                               printf out "## >>> Check \u{001b}[31mFAILED.\u{001b}[0m <<< (expected: %s %s %s)\n%!"
                                 (Nkexp.to_string e1) sgn (Nkexp.to_string e2);
--- lib/netkat/interp.mli
+++ lib/netkat/interp.mli
@@ -1,6 +1,6 @@
 (** The module for interpreting nkpl commands from strings. *)
 
-type result = Success | Fail of Trace.t option
+type result = Success of Trace.t option | Fail of Trace.t option
 
 (** Parse a string as a nkpl program. *)
 val parse_string : (string -> unit) -> Env.t -> string -> Nkcmd.t option
